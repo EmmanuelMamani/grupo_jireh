7bbd54b98872215ea297894ff10a146c51c8be0d
DafneXCA <cruzdafne123@gmail.com>
2022-07-31 20:42:09 -0400

    Arreglos de las relaciones

--- app/Models/Zona.php
+++ app/Models/Zona.php
@@ -2,7 +2,6 @@
 
 namespace App\Models;
 
-use Facade\FlareClient\Http\Client;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -10,6 +9,6 @@ class Zona extends Model
 {
     use HasFactory;
     public function clientes(){
-        return $this->hasMany(Client::class);
+        return $this->hasMany(Cliente::class);
     }
 }

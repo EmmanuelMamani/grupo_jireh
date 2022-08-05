b8640eec9bd06630af172fb81ffafc2a0019acf1
DafneXCA <cruzdafne123@gmail.com>
2022-08-05 17:45:07 -0400

    Botones del menu

--- resources/views/menu.blade.php
+++ resources/views/menu.blade.php
@@ -6,22 +6,27 @@
 @section("titulo", "Menu")
 @section("contenido")
 <div id="opciones">
-    <div class="opcion row">
-        <span class="funciones col-8">Clientes</span>
-        <span class="material-symbols-outlined icono col">group</span>
-    </div>
-    <div class="opcion row">
-        <span class="funciones col-8">Empleados</span>
-        <span class="material-symbols-outlined icono col"> business_center</span>
-    </div>
-    <div class="opcion row">
-        <span class="funciones col-8">Cuentas</span>
-        <span class="material-symbols-outlined icono col">monetization_on</span>
-    </div>
-    <div class="opcion row">
-        <span class="funciones col-8">Lotes</span>
-        <span class="material-symbols-outlined icono col">local_shipping</span>
-    </div>
+    @if (Auth::user()->Rol=='Administrador')
+        <div class="opcion row">
+            <span class="funciones col-8">Clientes</span>
+            <span class="material-symbols-outlined icono col">group</span>
+        </div>
+        <div class="opcion row">
+            <span class="funciones col-8">Empleados</span>
+            <span class="material-symbols-outlined icono col"> business_center</span>
+        </div>
+        <div class="opcion row">
+            <span class="funciones col-8">Cuentas</span>
+            <span class="material-symbols-outlined icono col">monetization_on</span>
+        </div>
+        <div class="opcion row">
+            <span class="funciones col-8">Lotes</span>
+            <span class="material-symbols-outlined icono col">local_shipping</span>
+        </div>
+    @endif
+   
+    
+    
     <a class="opcion row" href="/venta">
         <span class="funciones col-8">Pre-Venta</span>
         <span class="material-symbols-outlined icono col">shopping_cart</span>

dbcfa371297b707495cf253a285827a530024d49
Emmanuel Mamani <emmanuelmamani100@gmail.com>
2024-10-31 08:55:15 -0400

    fix pendientes

--- resources/views/ventas_pendientes.blade.php
+++ resources/views/ventas_pendientes.blade.php
@@ -37,7 +37,8 @@
         @endforeach
     </tbody>
 </table>
-<script src="https://code.jquery.com/jquery-3.5.1.js"></script>
+
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 <script src="https://cdn.datatables.net/1.11.5/js/jquery.dataTables.min.js"></script>
 <script src="https://cdn.datatables.net/1.11.5/js/dataTables.bootstrap5.min.js"></script>
 <script src="https://cdn.datatables.net/responsive/2.2.9/js/dataTables.responsive.min.js"></script>

b55cc83aaaebc0294b2818b62c5706173055effe
Emmanuel Mamani <emmanuelmamani100@gmail.com>
2024-10-31 08:57:29 -0400

    fix pendientes

--- resources/views/ventas_pendientes.blade.php
+++ resources/views/ventas_pendientes.blade.php
@@ -38,27 +38,27 @@
     </tbody>
 </table>
 
-<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<script src="https://code.jquery.com/jquery-3.5.1.js"></script>
 <script src="https://cdn.datatables.net/1.11.5/js/jquery.dataTables.min.js"></script>
 <script src="https://cdn.datatables.net/1.11.5/js/dataTables.bootstrap5.min.js"></script>
 <script src="https://cdn.datatables.net/responsive/2.2.9/js/dataTables.responsive.min.js"></script>
 <script src="https://cdn.datatables.net/responsive/2.2.9/js/responsive.bootstrap5.min.js"></script>
 <script>
-       $('#tabla').DataTable({
-    responsive:true,
-    autoWidth:false,
-    "language": {
-          "lengthMenu": "Mostrar _MENU_  ",
-          "zeroRecords": "No hay resultados",
-          "info": "Mostrando la página _PAGE_ de _PAGES_",
-          "infoEmpty": "No records available",
-          "infoFiltered": "(filtrado de _MAX_ registros totales)",
-          "search":"Buscar",
-          "paginate":{
-                "next":"Siguiente",
-                "previous":"Anterior"
-          }
-      }
-    });
+    $('#tabla').DataTable({
+ responsive:true,
+ autoWidth:false,
+ "language": {
+       "lengthMenu": "Mostrar _MENU_  ",
+       "zeroRecords": "No hay resultados",
+       "info": "Mostrando la página _PAGE_ de _PAGES_",
+       "infoEmpty": "No records available",
+       "infoFiltered": "(filtrado de _MAX_ registros totales)",
+       "search":"Buscar",
+       "paginate":{
+             "next":"Siguiente",
+             "previous":"Anterior"
+       }
+   }
+ });
 </script>
 @endsection
